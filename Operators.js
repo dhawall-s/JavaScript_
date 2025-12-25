4062b48a43ac76133dc3c75468aa5bf37db2659e
dhawall-s <sainidhawal755@gmail.com>
2025-12-25 15:04:06 +0530

    Operators

---
+++ Operators.js
@@ -0,0 +1,14 @@
+//Divide ->left to right
+//Multiply ->left to right
+//Add,Sub ->left to right
+//Modulus Operator -> Modulus gives the remainder
+//++Increment Operator , --Decrement Operator
+//Assignment Operator = assigns the value
+
+console.log(20 % 2);
+let sum = 20;
+console.log(sum);
+++sum;
+console.log(sum);
+sum++;
+console.log(sum);
