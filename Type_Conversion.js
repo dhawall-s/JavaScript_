0ee9d82190121c2852790f29bb50168f4a8326c2
dhawall-s <sainidhawal755@gmail.com>
2025-12-25 14:56:57 +0530

    Type_Conversions

---
+++ Type_Conversion.js
@@ -0,0 +1,36 @@
+let ac_bal = "2000";
+console.log("Type of ac_bal is ", typeof ac_bal);
+let num = Number(ac_bal);
+console.log("Type of ac_bal is after converting it", typeof num);
+
+let x = true;
+console.log(x);
+console.log(Number(x));
+let y = false;
+console.log(y);
+console.log(Number(y));
+
+//String containing alphs is not a number when converting it to number
+let account = "100xs";
+console.log(Number(account));
+
+let x1 = null;
+console.log(Number(x1));
+
+let x2 = undefined;
+console.log(Number(x2));
+
+let ab = 20;
+console.log(String(ab));
+console.log(typeof String(ab));
+
+let ax = true;
+console.log(ax);
+console.log(typeof String(ax));
+
+let abc = "";
+console.log(Boolean(abc));
+
+let abc1 = "true";
+console.log(Boolean(abc1));
+
