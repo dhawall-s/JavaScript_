b3885d50c68697726e2426f39b31330a3d6f87db
dhawall-s <sainidhawal755@gmail.com>
2025-12-29 11:30:55 +0530

    Object

---
+++ Object.js
@@ -0,0 +1,16 @@
+// object is a key value Pair
+const obj = {
+  naam: "Dhawal",
+  ac: 420,
+  gender: "Male",
+};
+console.log(obj);
+
+const inst = {
+  insta_user: "dhawal",
+  insta_pass: "12345",
+  
+};
+console.log(inst);
+console.log(inst["insta_pass"])
+
