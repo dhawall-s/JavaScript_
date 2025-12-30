fb3df3b592327a61d6dce15c1cd1aee9f2087bf3
dhawall-s <sainidhawal755@gmail.com>
2025-12-30 19:06:39 +0530

    Object

--- Object.js
+++ Object.js
@@ -46,3 +46,26 @@ let Per2 = new People("Dhawal", 21, "Male");
 console.log(Per1);
 console.log(Per2);
 
+let obje = {
+  name: "Rohit",
+  age: 30,
+  ac_bal: 345334,
+  gender: "Male",
+};
+const arr1 = Object.keys(obje);
+console.log(arr1);
+
+const arr2 = Object.values(obje);
+console.log(arr2);
+
+const arr3 = Object.entries(obj);
+console.log(arr3);
+
+let ob1 = { a: 1, b: 2 };
+let ob2 = { c: 3, d: 4 };
+let ob3 = { f: 5, g: 6 };
+let ob4 = Object.assign({}, ob1, ob2, ob3);
+console.log(ob4);
+
+const obj5 = { ...ob1, ...ob2, ...ob3 };
+console.log(obj5);
