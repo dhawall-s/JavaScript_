5f419b46abedb7797431e779f15781deceaf7830
dhawall-s <sainidhawal755@gmail.com>
2025-12-30 18:57:56 +0530

    Object

--- Object.js
+++ Object.js
@@ -9,8 +9,40 @@ console.log(obj);
 const inst = {
   insta_user: "dhawal",
   insta_pass: "12345",
-  
 };
 console.log(inst);
-console.log(inst["insta_pass"])
+console.log(inst["insta_pass"]);
+
+const arr = [1, 2, 3, 4, 5];
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+
+const person = new Object();
+
+person.name = "Dhawal";
+person.age = 80;
+person.gender = "Male";
+console.log(person);
+delete person.age;
+
+console.log(person);
+
+person.name = "Munna";
+console.log(person);
+
+class People {
+  constructor(na, ag, gen) {
+    this.na = na;
+    this.ag = ag;
+    this.gen = gen;
+  }
+}
+
+let Per1 = new People("Rohit", 20, "Male");
+let Per2 = new People("Dhawal", 21, "Male");
+console.log(Per1);
+console.log(Per2);
 
