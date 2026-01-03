6ad1e30ccb342728258ed5b0920069de19784f87
dhawall-s <sainidhawal755@gmail.com>
2026-01-03 16:28:16 +0530

    Function

--- Func.js
+++ Func.js
@@ -1,6 +1,27 @@
-function greet(){
-    console.log("Hello")
+function greet() {
+  console.log("Hello");
 }
+greet();
+//Parameters
+function sum(number1, number2) {
+  console.log(number1 + number2);
+}
+//Function Call Arguments
+sum("hello", "sum");
+sum(9, 8);
+
+function multiply(number1, number2) {
+  //   console.log(number1 * number2);
+  return number1 * number2;
+}
+let ans = multiply(4, 5);
+console.log(ans);
+
+const func1 = function () {
+  console.log("Hello Doston");
+};
+
+func1();
 
 
-greet()
+
