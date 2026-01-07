e2a5ae9a8a1bac5a92262d09e4bfd066fc80cdc7
dhawall-s <sainidhawal755@gmail.com>
2026-01-07 15:00:49 +0530

    DOM

---
+++ DOM.js
@@ -0,0 +1,16 @@
+//create element
+//append,prepend ->where element is wanted
+
+
+
+let h1=document.createElement("h1")
+h1.textContent="Hello"
+console.dir(h1)
+// document.body.append(h1)
+// document.body.prepend(h1)
+document.querySelector("body").prepend(h1)
+
+
+document.createElement
+
+
