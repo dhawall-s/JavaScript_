f8d4ec3f5da582a31b5193de45cda17242271a43
dhawall-s <sainidhawal755@gmail.com>
2026-01-07 14:12:38 +0530

    DOM

--- script.js
+++ script.js
@@ -9,6 +9,13 @@
 // document.getElementById
 // document.getElementsByClassName
 
-let abcd = document.querySelectorAll("h1")
+// let abcd = document.querySelectorAll("h1")
 // console.log(abcd)
-console.dir(abcd)
+// console.dir(abcd)
+
+let h1=document.querySelector("h1")
+// h1.innerText="Hello Dhawal"
+h1.innerHTML="<i>Hello</i>"
+console.dir(h1)
+
+
