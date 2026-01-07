fb4a5a969ab7526dfbad15a5122bc306bc0e7203
dhawall-s <sainidhawal755@gmail.com>
2026-01-07 20:33:22 +0530

    DOM

--- DOM.js
+++ DOM.js
@@ -3,14 +3,22 @@
 
 
 
-let h1=document.createElement("h1")
-h1.textContent="Hello"
-console.dir(h1)
+// let h1=document.createElement("h1")
+// h1.textContent="Hello"
+// console.dir(h1)
 // document.body.append(h1)
 // document.body.prepend(h1)
-document.querySelector("body").prepend(h1)
+// document.querySelector("body").prepend(h1)
 
 
-document.createElement
+
+let h1=document.querySelector("h1")
+// console.dir(h1)
+// h1.style.color="red"
+// h1.style.backgroundColor="pink"
+
+h1.classList.add("hulu");
+h1.classList.remove("hulu");
+h1.classList.toggle("hulu");
 
 
