726a99e69d0c751cf7e0742106df82a73ae74579
dhawall-s <sainidhawal755@gmail.com>
2026-01-09 13:18:00 +0530

    Events

---
+++ EventHandle.js
@@ -0,0 +1,21 @@
+//event matlab kuch action hua
+//event listener ka matlab us event ka reaction
+
+function bod(){
+    b.style.backgroundColor='aqua'
+}
+
+let h1=document.querySelector("h1")
+h1.addEventListener("click",function(){
+    h1.style.color="aqua";
+})
+
+let p=document.querySelector("p")
+p.addEventListener("dblclick",function(){
+    p.style.backgroundColor='green'
+})
+
+let b=document.querySelector("body")
+b.addEventListener("click",bod)
+
+b.removeEventListener("click",bod)
