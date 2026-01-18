632075306f37bdb9a6becc2d0644ec9f89af1fe5
dhawall-s <sainidhawal755@gmail.com>
2026-01-18 20:29:49 +0530

    Events

--- EventHandle.js
+++ EventHandle.js
@@ -8,7 +8,6 @@ h1.addEventListener("click",function(){
     h1.style.color="aqua";
 })
 let p=document.querySelector("p")
-
 p.addEventListener("dblclick",function(){
     p.style.backgroundColor='green'
 })
